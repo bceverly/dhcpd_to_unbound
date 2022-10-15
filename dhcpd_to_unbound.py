90f5af67c1a27c1ac312584d9190024f33ee2520
Mitchell Burton <mitch@mitchellburton.ca>
2022-10-15 12:00:02 -0700

    style and grace

--- dhcpd_to_unbound.py
+++ dhcpd_to_unbound.py
@@ -1,34 +1,34 @@
-#! /usr/local/bin/python3
+#!/usr/bin/env python3
 """
-    dhcpd_to_unbound
+dhcpd_to_unbound
 
-    A tool that helps integrate dhcpd to unbound by parsing the leases
-    file from dhcpd (normally in /var/db/dhcpd.leases) and converts it
-    to a series of blank-line separated A records using a domain name
-    specified on the command-line:
+A tool that helps integrate dhcpd to unbound by parsing the leases
+file from dhcpd (normally in /var/db/dhcpd.leases) and converts it
+to a series of blank-line separated A records using a domain name
+specified on the command-line:
 
-    dhcpd_to_unbound --input /var/db/dhcpd.leases --domain example.com
+dhcpd_to_unbound --input /var/db/dhcpd.leases --domain example.com
 """
 
-import re
 import argparse
 
+
 class Lease:
-""" 
+    """ 
     A class to manage the contents of a dhcpd lease record 
-"""
+    """
     hostname = None
     mac_address = None
     abandoned = None
 
-    def __init__(self,hostname,mac_address,abandoned):
+    def __init__(self, hostname=None, mac_address=None, abandoned=None):
         self.hostname = hostname
         self.mac_address = mac_address
         self.abandoned = abandoned
 
 
-def ParseFile(filename):
-"""
+def parse_file(filename):
+    """
     A function to parse the dhcpd.leases file
 
     Arguments:
@@ -37,77 +37,77 @@ def ParseFile(filename):
     Returns:
     A dictionary of Lease objects with the string of the ipv4 address
     as the key
-"""
-    file = open(filename, 'r')
-    Lines = file.readlines()
-
-    dict = {}
+    """
+    leases = {}
     current_ip = None
-    current_lease = Lease(None,None,None)
+    current_lease = Lease()
 
-    for line in Lines:
-        words = line.split(" ")
-        if line.startswith("lease"):
-            if current_ip == None:
-                # First time through
+    with open(filename) as fp:
+        for line in fp:
+            words = line.split(' ')
+
+            if line.startswith('lease'):
+                if current_ip is not None and current_ip not in leases:
+                    # Not the first time through.
+                    leases[current_ip] = current_lease
+                    current_lease = Lease(abandoned=False)
+
                 current_ip = words[1]
-            else:
-                if (dict.get(current_ip) == None):
-                    dict[current_ip] = current_lease
-                    current_lease = Lease(None,None,False)
-                current_ip = words[1]
-        elif len(words) > 0 and words[0].strip() == 'client-hostname':
-            hostname = words[1].strip()
-            hostname = re.sub('"','',hostname)
-            hostname = re.sub(';','',hostname)
-            current_lease.hostname = hostname
-        elif len(words) > 0 and words[0].strip() == 'hardware':
-            mac_address = words[2].strip()
-            mac_address = re.sub(';','',mac_address)
-            current_lease.mac_address = mac_address
-        elif len(words) > 0 and words[0].strip() == "abandoned;":
-            current_lease.abandoned = True
+            elif len(words) > 0 and words[0].strip() == 'client-hostname':
+                hostname = words[1].strip()
+                hostname = hostname.replace('"', '').replace(';', '')
+                current_lease.hostname = hostname
+            elif len(words) > 0 and words[0].strip() == 'hardware':
+                mac_address = words[2].strip()
+                mac_address = mac_address.replace(';', '')
+                current_lease.mac_address = mac_address
+            elif len(words) > 0 and words[0].strip() == 'abandoned;':
+                current_lease.abandoned = True
 
     # Add the last one
-    if dict.get(current_ip == None):
-        dict[current_ip] = current_lease
+    if current_ip not in leases:
+        leases[current_ip] = current_lease
 
-    return dict
+    return leases
 
 
-def WriteOutput(dict, domain_name):
-"""
-    Write out a list of blank line separated A records in a format used
-    by unbound from the list of lease objects
+def write_output(leases, domain_name):
+    """
+    Write out a list of blank line-separated A records in a format used
+    by unbound from the list of lease objects.
 
     Arguments:
-    dict - the dictionary of Lease objects produced by the Parse function
+    leases - the dictionary of Lease objects produced by the parse_file
         above
-    domain-name - the domain name (ex. example.org) that is to be appended
-        to the hostnames supplied by the Parse function in the dictionary
-"""
-    print("# DNS A records for active DHCP (dhcpd) leases")
-    print("#")
-    print("# File created using dhcpd_to_unbound (https://github.com/bceverly/dhcpd_to_unbound)")
-    print("")
-    for key in dict.keys():
-        if not dict[key].abandoned and not dict[key].hostname == None:
-            print('local-data: "{0}.{2}. IN A {1}"'.format(dict[key].hostname, key, domain_name))
-            print('local-data-ptr: "{0} {1}.{2}"'.format(key, dict[key].hostname, domain_name))
-            print('')
+    domain_name - the domain name (ex. example.org) that is to be appended
+        to the hostnames supplied by the parse_file in the dictionary
+    """
+    print('# DNS A records for active DHCP (dhcp) leases\n'
+          '#\n'
+          '# File created using dhcp_to_unbound '
+          '(https://github.com/bceverly/dhcp_to_unbound)\n')
+
+    for key, value in dict.items():
+        if not (value.abandoned or value.hostname is None):
+            print(f'local-data: "{value.hostname}.{domain_name}. IN A {key}"\n'
+                  f'local-data-ptr: "{key} {value.hostname}.{domain_name}"\n')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     # Parse the command line arguments
-    parser = argparse.ArgumentParser(description='Produce live unbound A records from the dhcpd leases data.')
-    parser.add_argument('-i','--input', metavar='filepath', type=str,
-            help='The dhcpd.leases file to be parsed', required=True,
-            dest='input_file')
-    parser.add_argument('-d','--domain', metavar='example.com', type=str,
-            required=True, help='The domain name to append to the hostnames',
-            dest='domain_name')
+    parser = argparse.ArgumentParser(description='Produce live unbound A '
+                                     'records from the dhcpd leases data.')
+    parser.add_argument(
+        '-i', '--input', metavar='filepath', type=str,
+        help='The dhcpd.leases file to be parsed', required=True,
+        dest='input_file')
+    parser.add_argument(
+        '-d', '--domain', metavar='example.com', type=str,
+        required=True, help='The domain name to append to the hostnames',
+        dest='domain_name')
+
     args = parser.parse_args()
 
     # Perform the translation
-    dict = ParseFile(args.input_file)
-    WriteOutput(dict, args.domain_name)
+    leases = parse_file(args.input_file)
+    write_output(leases, args.domain_name)
